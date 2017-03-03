3925dfe28af21db4cf5615548c6ea3a17e9fe76a
Rhitik Bhatt <bhattrhitik95@gmail.com>
2017-03-03 23:33:27 +0530

    Scaled the player

--- binder.py
+++ binder.py
@@ -12,7 +12,6 @@ def game():
 
     screen = pygame.display.set_mode(size)
     cowboy_sprite = pygame.image.load(os.path.abspath('assets/cowboy.png'))
-    cowboy_sprite = pygame.transform.scale2x(cowboy_sprite)
 
     while 1:
         for event in pygame.event.get():
@@ -31,9 +30,11 @@ def game():
         player1_surface = pygame.Surface((90, 90))
 
         # This is the area of image that we want to excerpt from the image cowboy.png
-        area_of_image = (player1.sprite_x, player1.sprite_y , 90, 90)
+        area_of_image = (player1.sprite_x, player1.sprite_y, 45, 45)
         # Draws the image of cowboy surface object for player 1
         player1_surface.blit(cowboy_sprite, (0, 0), area_of_image)
+        # Scaling the player 1's image
+        player1_surface = pygame.transform.scale(player1_surface, (180, 180))
         # Destination of the image to be drawn on main window
         dest = (player1.x, player1.y)
         # Draws the image that surface player1_surface contains onto the screen
