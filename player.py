87d1f0e90c0806f7d206866a757c635930d60592
Rhitik Bhatt <bhattrhitik95@gmail.com>
2017-03-04 10:31:51 +0530

    Removed clutter in binder.py

--- player.py
+++ player.py
@@ -56,6 +56,7 @@ class Player:
         self.x, self.y, self.action = coordinate_x, coordinate_y, action
         self.sprite_x, self.sprite_y = WIDTH_SPRITE * ACTIONS[action] [0], HEIGHT_SPRITE * ACTIONS[action] [1]
         self.direction = 'None'
+
     def __str__(self):
         """
         Returns the information about the player, helpful in debugging!
@@ -71,19 +72,46 @@ class Player:
         TODO: Change the action to walk
         """
         self.x += 1
+        # for action in ['walk-1', 'walk-2', 'walk-3', 'walk-4', 'walk-5']:
+        #     yield action
+
     def move_left(self):
         """Moves the player left
         TODO: Change the action to walk
         """
         self.x -= 1
-        
+
     def move_up(self):
         """Moves the player up
         TODO: Change the action to jump
         """
         self.y -= 1
-        
+
     def move_down(self):
         """Moves the player down
         """
         self.y += 1
+
+    def player_key_handler(self, pygame, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.direction = 'left'
+            elif event.key == pygame.K_RIGHT:
+                self.direction = 'right'
+            elif event.key == pygame.K_UP:
+                # TODO: Key Up handler can be used along
+                # with left and right keys add logic for that
+                self.direction = 'up'
+            elif event.key == pygame.K_DOWN:
+                self.direction = 'down'
+        elif event.type == pygame.KEYUP:
+            # if key is not pressed then direction will be set to none that is player will not move.
+            self.direction = 'None'
+        if self.direction == 'right':
+            self.move_right()
+        elif self.direction == 'left':
+            self.move_left()
+        elif self.direction == 'up':
+            self.move_up()
+        elif self.direction == 'down':
+            self.move_down()
